2feb162a0cadc047917836d995985f6266134e4d
user01357977 <hyperbolict@gmail.com>
2021-10-27 18:18:22 +0100

    add printOrderBook() function

--- derived.c
+++ derived.c
@@ -25,6 +25,68 @@ t_orderid replace(t_orderid orderid, t_order order) {
   return limit(order);
 }
 
+int printOrderBook(int numPriceLevels) {
+	orderBookEntry_t *bookEntry;
+	orderBookEntry_t *entry;
+	pricePoint_t *ppEntry;
+
+	ppEntry = pricePoints + bidMax + numPriceLevels;
+	t_size size = 0;
+	int count_ord;
+
+	int l = 0; while (l < numPriceLevels) {
+		if (bidMax + l >= MAX_PRICE) break;
+		size = 0;
+		count_ord = 0;
+		bookEntry = ppEntry->listHead;
+		while (bookEntry != NULL) {
+			size += bookEntry->size;
+			if (bookEntry->size > 0 ) count_ord++;
+			bookEntry = bookEntry->next;
+		}
+		if (count_ord != ppEntry->numOrders) {
+			printf("%d : %lu | %d vs %lu\n", (int) (bidMax + numPriceLevels - l), size, count_ord, ppEntry->numOrders);
+			printf("size zero counter: %d\n", counter_of_size_zero);
+			return 1;
+		}
+		if (size) {
+			printf("%d : %lu | %d vs %lu\n", (int) (bidMax + numPriceLevels - l), size, count_ord, ppEntry->numOrders);
+			if (l==4) printf("-----------------------------\n");
+		}
+		l++;
+		ppEntry--;
+
+	}
+
+	//printf("----------------------\n");
+	ppEntry = pricePoints + bidMax;
+
+	l = 0; while (l < numPriceLevels) {
+		size = 0;
+		count_ord = 0;
+		bookEntry = ppEntry->listHead;
+		while (bookEntry != NULL) {
+			size += bookEntry->size;
+			if (bookEntry->size > 0 ) count_ord++;
+			bookEntry = bookEntry->next;
+		}
+		if (count_ord != ppEntry->numOrders) {
+			printf("%d : %lu | %d vs %lu\n", (int) (bidMax + l), size, count_ord, ppEntry->numOrders);
+			printf("size zero counter: %d\n", counter_of_size_zero);
+			return 1;
+		}
+		if (size) {
+			printf("%d : %lu | %d vs %lu\n", (int) (bidMax + l), size, count_ord, ppEntry->numOrders);
+		}
+		l++;
+		ppEntry--;
+		if (bidMax - l  <= MIN_PRICE) break;
+	}
+
+	return 0;
+
+}
+
 /* An order type that is guaranteed to add liquidity
    used by market makes and rebate arbitrageurs
    IN: order: price will be ignored
